746ac8ac18329414e3b7a8ac44be129ebb5306f7
sanblchsun <logovopost@yandex.ru>
2017-09-24 23:05:55 +0300

    refactoring

---
+++ Temp/src/temp/Figure.java
@@ -0,0 +1,6 @@
+
+package temp;
+
+public enum Figure {
+    X,O
+}
--- Temp/src/temp/Temp.java
+++ Temp/src/temp/Temp.java
@@ -1,9 +1,24 @@
 package temp;
 
+import java.util.function.Predicate;
+
 public class Temp {
 
-    public static void main(String[] args) {
-        
+    public static boolean victory(Figure[][] figure, Predicate<Figure> pre) {
+        for(int i=0;i<figure.length;++i){
+            if(pre.test(figure[i][0]) && pre.test(figure[i][1]) && pre.test(figure[i][2])) {System.out.println(i + "" + 0); return true;}    
+            else if(pre.test(figure[0][i]) && pre.test(figure[1][i]) && pre.test(figure[2][i])) {System.out.println(0 + "" + i); return true;}
+        }  
+        if(pre.test(figure[1][1]) && (pre.test(figure[0][0]) && pre.test(figure[2][2]) || 
+                (pre.test(figure[0][2]) && pre.test(figure[2][0])))) {System.out.println("Крест"); return true;}
+        return false;
     }
     
+   public static void main(String[] args) {
+       Figure[][] figure = {{Figure.O,null,Figure.X},
+                            {Figure.O,Figure.X,Figure.X},
+                            {Figure.O,null,Figure.X}}; 
+       
+       System.out.println(victory(figure, (x)->x==Figure.X));
+  }
 }
